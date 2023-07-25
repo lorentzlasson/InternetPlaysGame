20945b02c0cb760abf9aeaf76687b5034287d7b0
Lorentz Lasson <lasson.lorentz@gmail.com>
2023-07-25 16:13:11 +0200

    Link to github from info

--- src/ui/info.tsx
+++ src/ui/info.tsx
@@ -14,67 +14,80 @@ const ui = () => {
         </meta>
       </head>
       <body>
+        <div>
+          <a
+            href='javascript:history.back()'
+            style={{
+              textDecoration: 'none',
+            }}
+          >
+            🔙
+          </a>
+          <div>
+            A game inspired by{' '}
+            <a href='https://en.wikipedia.org/wiki/Wordle'>
+              Wordle
+            </a>
+            {', '}
+            <a href='https://en.wikipedia.org/wiki/Twitch_Plays_Pok%C3%A9mon'>
+              Twitch Plays Pokémon
+            </a>
+            {' and '}
+            <a href='https://en.wikipedia.org/wiki/Snake_(video_game_genre)'>
+              Snake
+            </a>. Vote on which direction to move. How high can the internet
+            score?
+          </div>
+          <br />
+          <div>
+            🏃 The avatar controlled by the players
+          </div>
+          <div style={{ display: 'flex' }}>
+            <div style={{ opacity: '0.2' }}>
+              🏃
+            </div>{' '}
+            The previous position of the avatar
+          </div>
+          <div>
+            🪙 Awards one point when stepped on
+          </div>
+          <div>
+            💣 Resets the points to 0 when stepped on
+          </div>
+          <div>
+            ⬜ Does nothing when stepped on
+          </div>
+          <div>
+            ⬅️⬇️⬆️➡️ Voting buttons. Only one vote per player per day. Once voted,
+            the opacity hints the total vote distribution
+          </div>
+          <div>
+            ⌛Time until next move
+          </div>
+          <div>
+            🪙🥇 Current score and high score
+          </div>
+          <div>
+            🗳️ Your vote
+          </div>
+          <div>
+            👤➡️🚪 Sign in
+          </div>
+          <div>
+            🗣️ Share your vote
+          </div>
+        </div>
+
         <a
-          href='javascript:history.back()'
+          href='https://github.com/lorentzlasson/internetplaysgame'
           style={{
+            position: 'fixed',
+            bottom: '10px',
             textDecoration: 'none',
           }}
         >
-          🔙
+          🐙🐈
         </a>
-        <div>
-          A game inspired by{' '}
-          <a href='https://en.wikipedia.org/wiki/Wordle'>
-            Wordle
-          </a>
-          {', '}
-          <a href='https://en.wikipedia.org/wiki/Twitch_Plays_Pok%C3%A9mon'>
-            Twitch Plays Pokémon
-          </a>
-          {' and '}
-          <a href='https://en.wikipedia.org/wiki/Snake_(video_game_genre)'>
-            Snake
-          </a>. Vote on which direction to move. How high can the internet
-          score?
-        </div>
-        <br />
-        <div>
-          🏃 The avatar controlled by the players
-        </div>
-        <div style={{ display: 'flex' }}>
-          <div style={{ opacity: '0.2' }}>
-            🏃
-          </div>{' '}
-          The previous position of the avatar
-        </div>
-        <div>
-          🪙 Awards one point when stepped on
-        </div>
-        <div>
-          💣 Resets the points to 0 when stepped on
-        </div>
-        <div>
-          ⬜ Does nothing when stepped on
-        </div>
-        <div>
-          ⬅️⬇️⬆️➡️ Voting buttons. Only one vote per player per day. Once voted, the
-          opacity hints the total vote distribution
-        </div>
-        <div>
-          ⌛Time until next move
-        </div>
-        <div>
-          🪙🥇 Current score and high score
-        </div>
-        <div>
-          🗳️ Your vote
-        </div>
-        <div>
-          👤➡️🚪 Sign in
-        </div>
-        <div>
-          🗣️ Share your vote
-        </div>
       </body>
     </html>
   );
