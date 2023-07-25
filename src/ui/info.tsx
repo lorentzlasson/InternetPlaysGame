f1c734afb33ad564265b69a85e452085f338fc29
Lorentz Lasson <lasson.lorentz@gmail.com>
2023-07-25 14:36:45 +0200

    Credit snake

--- src/ui/info.tsx
+++ src/ui/info.tsx
@@ -38,10 +38,14 @@ const ui = () => {
           A game inspired by{' '}
           <a href='https://en.wikipedia.org/wiki/Wordle'>
             Wordle
-          </a>{' '}
-          and{' '}
+          </a>
+          {', '}
           <a href='https://en.wikipedia.org/wiki/Twitch_Plays_Pok%C3%A9mon'>
             Twitch Plays Pokémon
+          </a>
+          {' and '}
+          <a href='https://en.wikipedia.org/wiki/Snake_(video_game_genre)'>
+            Snake
           </a>. Vote on which direction to move. How high can the internet
           score?
         </div>
